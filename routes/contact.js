7434f97ca28080bb936a5a5f8774f187b0e99339
LeastActionCompany <leastactioncompany@gmail.com>
2026-06-14 13:53:36 +0530

    name changed from er to re

--- routes/contact.js
+++ routes/contact.js
@@ -9,7 +9,7 @@ router.get('/', (req, res) => {
       landmark: 'Near Arulmigu Mariyamman And Padavettamman Thirukovil',
       phone: '94860 91662',
       email: 'velloreroyalcoaching@gmail.com',
-      website: 'www.royalcoachingcenter.com',
+      website: 'www.royalcoachingcentre.com',
       mapEmbed: 'https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3888.000000000000!2d79.13!3d12.92!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMTLCsDU1JzEyLjAiTiA3OcKwMDcnNDguMCJF!5e0!3m2!1sen!2sin!4v1234567890',
       hours: {
         weekdays: '7:00 AM – 8:00 PM',
